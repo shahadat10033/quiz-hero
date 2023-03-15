349b7358e507425b5046c971700789eb442dd919
shahadat10033 <shahadat10033@gmail.com>
2023-03-16 00:22:30 +0600

    showing 6 undefined question with 4 option

--- index.js
+++ index.js
@@ -46,9 +46,10 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
   displayQuiz(data);
+  console.log(data);
 };
 
 // Displaying quiz on quiz page
@@ -74,7 +75,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
