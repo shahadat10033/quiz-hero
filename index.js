5e1d6cb682952c05c08ed6f60f1659b3efda73c2
shahadat10033 <shahadat10033@gmail.com>
2023-03-16 01:14:01 +0600

    fixing a silly bug of local storage and showing the submission result

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ document.querySelector("#submit").addEventListener("click", () => {
   }
 
   // data setting on local storage and getting data from local storage
-  let storage = JSON.parse(localStorage.getItem("result"));
+  let storage = JSON.parse(localStorage.getItem("results"));
   if (storage) {
     localStorage.setItem(
       "results",
@@ -120,6 +120,7 @@ document.querySelector("#submit").addEventListener("click", () => {
           examTime: timeTaken.innerText,
           status: grade.status,
         },
+        console.log(...storage),
       ])
     );
   } else {
